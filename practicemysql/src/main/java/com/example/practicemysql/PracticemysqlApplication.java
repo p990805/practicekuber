b076a4e3d9ab654883e9dda24cded2af98298258
박주찬 <p990805@naver.com>
2025-03-08 01:40:51 +0900

    [주찬] Feat : 쿠버네티스 연습 03.08

---
+++ practicemysql/src/main/java/com/example/practicemysql/PracticemysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicemysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticemysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticemysqlApplication.class, args);
+	}
+
+}
